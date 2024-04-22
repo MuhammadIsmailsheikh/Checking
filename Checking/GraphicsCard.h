ca16a5283e58fda4135643e616ac724789476970
MuhammadIsmailsheikh <167533291+MuhammadIsmailsheikh@users.noreply.github.com>
2024-04-22 22:13:42 +0500

    Update GraphicsCard.h

--- Checking/GraphicsCard.h
+++ Checking/GraphicsCard.h
@@ -1,102 +1,23 @@
-#include "GraphicsCard.h"
-#include<string>
-#include"Model.h"
-#include"Nvidia.h"
-#include"AMD.h"
-#include"AppleGPU.h"
+#pragma once
 using namespace std;
-#include<iostream>
-GraphicsCard::GraphicsCard() {
-	brand = "";
-	memorySize = 0;
-	price = 0;
-}GraphicsCard::GraphicsCard(string s, int memorysize, double price) {
-	// Assuming brand has enough capacity to hold the brand name
-	for (size_t i = 0; i < s.length(); i++) {
-		this->brand += s[i];
-	}
+#include<string>
+class GraphicsCard
+{
+protected:
+	string brand;
+	int memorySize;
+	double price;
+public:
+	GraphicsCard();
+	GraphicsCard(string brand, int memorysize, double price);
+	void setbrand(string s);
+	void setmemorysize(int s);
+	void setprice(double p);
+	string getbrand();
+	int germemorysize();
+	double getprice();
+	void show();
 
-	this->memorySize = memorysize;
-	this->price = price;
-}
+	static GraphicsCard* userimplementation(int g);
+};
 
-void GraphicsCard::setbrand(string s) {
-	// Assuming brand has enough capacity to hold the new brand name
-	this->brand = ""; // Clear the brand string
-	for (size_t i = 0; i < s.length(); i++) {
-		this->brand += s[i];
-	}
-}
-
-void GraphicsCard::setmemorysize(int s) {
-	this->memorySize = s;
-}
-void GraphicsCard::setprice(double p) {
-	this->price = price;
-}
-string GraphicsCard::getbrand() {
-	return brand;
-}
-int GraphicsCard::germemorysize() {
-	return memorySize;
-}
-double GraphicsCard::getprice() {
-	return price;
-}
-void GraphicsCard::show() {
-	cout << "Brand of GraphicCard is :" << this->brand << endl;
-	cout << "size of GraphicCard is :" << this->memorySize<<"GB" << endl;
-	cout << "price of GraphicCard is :" <<"Rs :" << this->price << endl;
-
-}
-
-GraphicsCard* GraphicsCard::userimplementation(int g) {
-	string brand1;
-	int size;
-	double priceg;
-	int c;
-	int c1;
-	Model m;
-	GraphicsCard* MeraGraphiccard = nullptr;
-	
-	cout << "      GraphicCard\n" << endl;
-	if (g == 1) {
-		cout << "Enter Brand of Graphic Card" << endl;
-		cin >> brand1;
-		while (brand1 != "Nvidia" && brand1 != "AMD") {
-			if (brand1 != "Nvidia" || brand1 != "AMD") {
-				cout << " Incorrect card!please enter Nvidia or AMD " << endl;
-				cin >> brand1;
-			}
-		}
-		cout << "Enter size of Graphic Card" << endl;
-		cin >> size;
-		while (size < 2 || size >24) {
-			if (size < 2 || size>24) {
-				cout << " Incorrect card size! Enter size between 1 to 24" << endl;
-				cin >> size;
-			}
-		}
-		//	cout << "Enter price of Graphic Card" << endl;
-			//cin >> priceg;
-		if (brand1 == "AMD") {
-			AMD* MeraCard = new AMD(brand1, size, 30000);
-			MeraGraphiccard = MeraCard;
-			return MeraGraphiccard;
-
-		}
-		if (brand1 == "Nvidia") {
-			Nvidia* MeraCard = new Nvidia(brand1, size, 30000);
-			MeraGraphiccard = MeraCard;
-			return MeraGraphiccard;
-		}
-	}
-
-	if(g==2) {
-		cout << "Apple has integrated GPU inside CPU" << endl;
-		
-			
-		
-		
-	}
-}
